113e5d5689c3aee6f3eb9e1d8a053957cc76e261
sungf <sungf@tinckay.com>
2018-10-11 18:31:25 +0800

    功能已完成，余留问题，将canvas转为图片有些模糊

--- js/draggable.js
+++ js/draggable.js
@@ -1,6 +1,8 @@
 var mouseX = 0, mouseY = 0;//鼠标的起始（点击）位置
 var targetX = 0, targetY = 0;//dom元素的起始位置
 var isMouseDown = false;//是否按下
+var minRadius = 15; //最小圆半径为15
+
 function mouseDown(evt){
 	if(!isOnResize){
 		var el = evt.currentTarget;
@@ -14,22 +16,20 @@ function mouseDown(evt){
 	return false;
 }
 function mouseMove(evt){
-	if(isMouseDown){
-		var el = evt.currentTarget;
-		var offsetX = evt.screenX - mouseX, offsetY = evt.screenY - mouseY;
-		el.style.left = (targetX + offsetX) + 'px';
-		el.style.top = (targetY + offsetY) + 'px';
-		console.log('left:'+el.style.left+'------top:'+el.style.top)
-		
-		var img = document.getElementById('dynamic_image_id');
-		img.style.left = -(targetX + offsetX) + 'px';
-		img.style.top = -(targetY + offsetY) + 'px';
-		
-		previewImage()
+	if(!isMouseDown && !isOnResize){
+		return false;
 	}
+	if(isMouseDown){
+		moveCircle(evt);
+	}
+	if(isOnResize){
+		resizeCircle(evt);
+	}
+	previewImage()
 }
-function mouseUp(evt){
+function mouseUp(){
 	isMouseDown = false;
+	isOnResize = false;
 }
 
 function getNumberFormPixel(pixel){
@@ -41,33 +41,85 @@ function getNumberFormPixel(pixel){
 	return typeof result === 'number' ? result : 0;
 }
 
-
-
 /*change circle size*/
 var isOnResize = false;
 function resize_mouseDown(evt){
-	isOnResize = true;
 	mouseX = evt.screenX;
 	mouseY = evt.screenY;
-//	evt.preventDefault();
-//	return false;
-}
-function resize_mouseMove(evt){
-	if(isOnResize){
-		var offsetX = evt.screenX - mouseX, offsetY = evt.screenY - mouseY;//鼠标的偏移
-		var offsetValue = Math.min(offsetX, offsetY);//取最小值
-		var circleEl = document.getElementById('dynamic_circle_box_id');
-		circleEl.style.width = circleEl.clientWidth + offsetValue + 'px';
-		circleEl.style.height = circleEl.clientHeight + offsetValue + 'px';
-		mouseX = evt.screenX;
-		mouseY = evt.screenY;
-	}
-}
-function resize_mouseUp(evt){
-	isOnResize = false;
+	isOnResize = true;
+	isMouseDown = false;
+	evt.preventDefault();
+	return false;
 }
 
-/*如果鼠标在遮罩上移动，则重置isOnResize*/
-function clearMouseState(){
-//	isOnResize = false;
+/*移动圆形位置*/
+function moveCircle(evt){
+	var el = document.getElementById('dynamic_circle_box_id');
+	var offsetX = evt.screenX - mouseX, offsetY = evt.screenY - mouseY;
+	/*上边界*/
+	if(targetY + offsetY < 0){
+		offsetY = - targetY;
+	}
+	/*右边界*/
+	if(el.clientWidth + targetX + offsetX > limitWidth){
+        offsetX = limitWidth - (el.clientWidth + targetX);
+	}
+	/*下边界*/
+	if(el.clientHeight + targetY + offsetY > limitHeight){
+		offsetY = limitHeight - (el.clientHeight + targetY);
+	}
+	/*左边界*/
+	if(targetX + offsetX < 0){
+		offsetX = -targetX;
+	}
+	el.style.left = (targetX + offsetX) + 'px';
+	el.style.top = (targetY + offsetY) + 'px';
+	console.log('left:'+el.style.left+'------top:'+el.style.top)
+	
+	var img = document.getElementById('dynamic_image_id');
+	img.style.left = -(targetX + offsetX) + 'px';
+	img.style.top = -(targetY + offsetY) + 'px';
+}
+/*改变圆形大小*/
+function resizeCircle(evt){
+	var circleEl = document.getElementById('dynamic_circle_box_id');
+	var maskEl = document.getElementById('mask_id');
+	var offsetX = evt.screenX - mouseX, offsetY = evt.screenY - mouseY;
+	var offsetRadius = 0;
+	
+	mouseX = evt.screenX;
+	mouseY = evt.screenY;
+	
+	//计算增加或减少的半径
+	switch (true){
+		case offsetX > 0 && offsetY > 0:
+			offsetRadius = Math.max(offsetX, offsetY);
+			break;
+		case offsetX < 0 && offsetY < 0:
+			offsetRadius = Math.min(offsetX, offsetY);
+			break;
+		case offsetX > 0 && offsetY < 0:
+			offsetRadius = Math.min(offsetX, offsetY);
+			break;
+		case offsetX < 0 && offsetY > 0:
+			offsetRadius = Math.max(offsetX, offsetY);
+			break;
+		default:
+			break;
+	}
+	//最小圆半径控制
+	if(offsetRadius < 0 && circleEl.clientWidth + offsetRadius < minRadius){
+		circleEl.style.width = minRadius + 'px';
+		circleEl.style.height = minRadius + 'px';
+		return true;
+	}
+	//边界处理
+	if(circleEl.offsetLeft + circleEl.clientWidth + offsetRadius <= maskEl.clientWidth && circleEl.offsetTop + circleEl.clientHeight + offsetRadius <= maskEl.clientHeight){
+		circleEl.style.width = circleEl.clientWidth + offsetRadius + 'px';
+		circleEl.style.height = circleEl.clientHeight + offsetRadius + 'px';
+	}else if(circleEl.offsetLeft + circleEl.clientWidth + offsetRadius > maskEl.clientWidth){
+		circleEl.style.width = maskEl.clientWidth - circleEl.offsetLeft + 'px';
+	}else if(circleEl.offsetTop + circleEl.clientHeight + offsetRadius > maskEl.clientHeight){
+		circleEl.style.height = maskEl.clientHeight - circleEl.offsetTop + 'px';
+	}
 }
--- js/drawImage.js
+++ js/drawImage.js
@@ -1,27 +1,12 @@
 /*显隐头像处理窗口*/
 function displayDealBox(isShow){
 	if(typeof isShow === 'boolean'){
-		var dealBoxEl = document.getElementById('select_img_box_id');
-		dealBoxEl.style.display = isShow ? 'inline-flex' : 'none';
-	}
-}
-
-/*根据图片路径判断是否展示图像*/
-function displayImage(imgUrl){
-	var selectImgList = document.getElementsByClassName('selected_img');
-	var hasImg = document.getElementById('has_img_id');
-	var emptyImg = document.getElementById('empty_img_id');
-	if(imgUrl){
-		for(var i = 0, len = selectImgList.length; i < len; i++){
-			selectImgList[i].src = imgUrl;
+		if(isShow){
+			document.getElementById('has_img_id').style.display = "none";
+			document.getElementById('empty_img_id').style.display = "block";
 		}
-		hasImg.style.display = 'block';
-		emptyImg.style.display = 'none';
-	}else {
-		hasImg.style.display = 'none';
-		emptyImg.style.display = 'block';
+		document.getElementById('select_img_box_id').style.display = isShow ? 'inline-flex' : 'none';
 	}
-	previewImage()
 }
 
 /*打开图片选择框*/
@@ -35,11 +20,86 @@ function selectImage(file){
 	if(!file.files || !file.files.length){
 		return false;
 	}
+	
+	var errMsg = '';
+	if(file.files[0].size / 1.0 / 1024 / 1024 > 1){
+		errMsg = "图片大小不能超过1M";
+		alert(errMsg);
+		return false;
+	}
+	//读取图片的数据
 	var reader = new FileReader();
 	reader.onload = function(evt){
-		displayImage(evt.target.result)
+		//加载图片获取图片真实宽度和高度
+		var img = new Image();
+		img.onload = function(){
+			if(this.width <= 50){
+				errMsg = "图片宽度必须大于50";
+			}
+			if(this.height <= 50){
+				errMsg = errMsg ? "图片的宽高必须大于50" : "图片高度必须大于50";
+			}
+			if(errMsg){
+				alert(errMsg);
+		        return false;
+			}
+			displayImage(evt.target.result, this.width, this.height);
+		}
+		img.src = evt.target.result;
 	}
 	reader.readAsDataURL(file.files[0]);
+}
+
+var limitWidth = 0, limitHeight = 0;
+
+/*根据上传的图片调整样式*/
+function displayImage(imgUrl, imgWidth, imgHeight){
+	var selectImgList = document.getElementsByClassName('selected_img');
+	var hasImg = document.getElementById('has_img_id');
+	var emptyImg = document.getElementById('empty_img_id');
+	var mask = document.getElementById('mask_id');
+	var circle = document.getElementById('dynamic_circle_box_id');
+	if(imgUrl){
+		//根据图片真实宽高调整图片、遮罩和裁剪圆的样式
+		var width = 0, height = 0;//图片实际显示宽高
+		var ratio = imgWidth * 1.0 / imgHeight;
+		if(imgWidth > imgHeight){
+			width = imgWidth > 360 ? 360 : imgWidth;
+			height = width / ratio;
+		}else if(imgWidth < imgHeight) {
+			height = imgHeight > 360 ? 360 : imgHeight;
+			width = height * ratio;
+		}else {
+			width = 360;
+			height = 360;
+		}
+		for(var i = 0, len = selectImgList.length; i < len; i++){
+			selectImgList[i].src = imgUrl;
+			selectImgList[i].style.width = width >= height ? width + 'px' : 'auto';
+			selectImgList[i].style.height = height >= width ? height + 'px' : 'auto';
+			selectImgList[i].style.left = '0px';
+			selectImgList[i].style.top = '0px';
+		}
+		
+		mask.style.width = width >= height ? width + 'px' : '100%';
+		mask.style.height = height >= width ? height + 'px' : '100%';
+		
+		//重置截取圆的位置，并取最小值最为宽高
+		var minLength = width > height ? height : width;
+		circle.style.width = minLength + 'px';
+		circle.style.height = minLength + 'px';
+		circle.style.left = '0px';
+		circle.style.top = '0px';
+		limitWidth = width;
+		limitHeight = height;
+		
+		hasImg.style.display = 'grid';
+		emptyImg.style.display = 'none';
+	}else {
+		hasImg.style.display = 'none';
+		emptyImg.style.display = 'block';
+	}
+	previewImage()
 }
 
 /*根据当前图片的选中区域预览裁剪部分*/
@@ -47,13 +107,45 @@ function previewImage(){
 	var dynamicCircleEl = document.getElementById('dynamic_circle_box_id');
 	var sourceImg = document.getElementById('dynamic_image_id');
 	var ratio = sourceImg.naturalHeight / sourceImg.clientHeight;//注意：图片的原始尺寸/放置的尺寸
-	drawImage('dynamic_image_id', 'canvas_100_id', dynamicCircleEl.offsetLeft * ratio, dynamicCircleEl.offsetTop * ratio, sourceImg.clientHeight, sourceImg.clientHeight, 0, 0, 100, 100);
-	drawImage('dynamic_image_id', 'canvas_50_id', dynamicCircleEl.offsetLeft * ratio, dynamicCircleEl.offsetTop * ratio, sourceImg.clientHeight, sourceImg.clientHeight, 0, 0, 50, 50);
+	
+	var dest_100 = document.getElementById('canvas_100_id');
+	var ctx_100 = dest_100.getContext("2d");
+	ctx_100.drawImage(sourceImg,dynamicCircleEl.offsetLeft * ratio, dynamicCircleEl.offsetTop * ratio, dynamicCircleEl.clientWidth * ratio, dynamicCircleEl.clientHeight * ratio, 0, 0, 100, 100);
+	
+	var dest_50 = document.getElementById('canvas_50_id');
+	var ctx_50 = dest_50.getContext("2d");
+	ctx_50.drawImage(sourceImg,dynamicCircleEl.offsetLeft * ratio, dynamicCircleEl.offsetTop * ratio, dynamicCircleEl.clientWidth * ratio, dynamicCircleEl.clientHeight * ratio, 0, 0, 50, 50);
 }
 
-function drawImage(sourceId, destId, sx, sy, swidth, sheight, x, y, width, height){
+/*剪裁图片绘制，此处封装成方法会出错，待探究*/
+function drawCanvas(sourceId, destId, sx, sy, swidth, sheight, x, y, width, height){
 	var destCanvas = document.getElementById(destId);
 	var ctx = destCanvas.getContext("2d");
 	var sourceImg = document.getElementById(sourceId);
-	ctx.drawImage(sourceImg, sx, sy, swidth, sheight, x, y, width, height);
+	ctx.drawImage(sourceImg,  x, y, width, height);
+}
+
+function uploadImage(){
+	var head_photo = document.getElementById('header_photo_id');
+	var canvas_100 = document.getElementById('canvas_100_id');
+//	var dom  = document.getElementsByClassName('img-box')[0];
+	var _image = new Image();
+	_image = canvas_100.toDataURL("image/png");
+	
+//	canvas_100.toBlob(function(blob) {
+//	  var newImg = document.createElement("img"),
+//	      url = URL.createObjectURL(blob);
+//	
+//	  newImg.onload = function() {
+//	    // no longer need to read the blob so it's revoked
+//	    URL.revokeObjectURL(url);
+//	  };
+//	
+//	  newImg.src = url;
+//	  dom.appendChild(newImg);
+//	});
+	
+	displayDealBox(false);
+	head_photo.src = _image;
+	head_photo.style.display = 'block'
 }
